280ac74d70c0d770fe05adf7c8ebd0764e06a6ba
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2015-11-01 18:41:07 +0000

    Add test commit

--- Main/src/pack/MainClass.java
+++ Main/src/pack/MainClass.java
@@ -5,6 +5,11 @@ package pack;
  */
 public class MainClass {
     public static void main(String[] args) {
+        test();
         System.out.println("Hello team x");
+    }
+
+    public static void test() {
+        System.out.println("Test");
     }
 }
