b7bdfffb2120261cc74c650bca5a00441338c47b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2015-11-01 18:26:22 +0000

    Added ignore rules.

--- Main/src/pack/MainClass.java
+++ Main/src/pack/MainClass.java
@@ -5,6 +5,6 @@ package pack;
  */
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello Team X");
+        System.out.println("Hello team x");
     }
 }
