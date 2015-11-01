ea4258ae3b0e0ec487534734194cd761b1fbe54d
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2015-11-01 16:49:39 +0000

    Intellij project

---
+++ Main/src/Origin/Hello.java
@@ -0,0 +1,10 @@
+package Origin;
+
+/**
+ * Created by 2090927d on 01/11/15.
+ */
+public class Hello {
+    public static void main (String [] args){
+        System.out.println("Hello");
+    }
+}
