38a690471f91d1f2d843b126aed4b7a93959a417
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2015-11-01 18:21:03 +0000

    Added main project.

---
+++ Main/src/pack/MainClass.java
@@ -0,0 +1,10 @@
+package pack;
+
+/**
+ * Created by andrei on 11/1/15.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Team X");
+    }
+}
