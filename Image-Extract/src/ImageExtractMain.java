7439de75d90e65b71ad7fe51f6992ff467fefbb9
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-01-26 15:16:50 +0000

    Finished the image extraction tool. It just needs to be integrated with the whole system.

--- Image-Extract/src/ImageExtractMain.java
+++ Image-Extract/src/ImageExtractMain.java
@@ -1,15 +1,13 @@
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFPictureData;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.zip.Adler32;
-import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -25,6 +23,8 @@ public class ImageExtractMain {
         chooser.setFileFilter(filter);
         chooser.setMultiSelectionEnabled(false);
         int returnVal = chooser.showOpenDialog(null);
+        /* this allows to select .docx file via clicking on a GUI
+                   - will be removed when integrated -        */
         if(returnVal == JFileChooser.APPROVE_OPTION) {
             File file=chooser.getSelectedFile();
             System.out.println(file.toString());
@@ -36,18 +36,6 @@ public class ImageExtractMain {
 
     public static void extractImages(String src) {
         try{
-            // initializing the zip archive
-            int BUFFER = 2048;
-            BufferedInputStream origin = null;
-            FileOutputStream dest = new
-                    FileOutputStream("D:/Desktop/imagesArchive.zip");
-            CheckedOutputStream checksum = new
-                    CheckedOutputStream(dest, new Adler32());
-            ZipOutputStream out = new
-                    ZipOutputStream(new
-                    BufferedOutputStream(checksum));
-            byte data[] = new byte[BUFFER];
-
             //create file inputstream to read from a binary file
             FileInputStream is = new FileInputStream(src);
             //create office word 2007+ document object to wrap the word file
@@ -56,50 +44,65 @@ public class ImageExtractMain {
             List<XWPFPictureData> piclist=docx.getAllPictures();
             //traverse through the list and write each image to a file
             Iterator<XWPFPictureData> iterator=piclist.iterator();
+            // list of strings containing the paths to the images
+            List<String> srcFiles = new ArrayList<String>();
+
             int i=0;
             while(iterator.hasNext()){
                 XWPFPictureData pic=iterator.next();
                 byte[] bytepic=pic.getData();
                 BufferedImage imag = ImageIO.read(new ByteArrayInputStream(bytepic));
+                // parsing through each image, checking if type is either PNG or JPEG, then
+                // writing it on disk and adding it to the list of paths
                 switch (pic.getPictureType()) {
                     case 6:
                         File pngImage = new File("D:/Desktop/imageFromWord" + i + ".png");
                         ImageIO.write(imag, "png", pngImage);
-                        FileInputStream fi = new FileInputStream(pngImage);
-                        origin = new BufferedInputStream(fi, BUFFER);
-                        ZipEntry entry = new ZipEntry(pngImage.toString());
-                        out.putNextEntry(entry);
-                        int count;
-                        while ((count = origin.read(data, 0,
-                                BUFFER)) != -1) {
-                            out.write(data, 0, count);
-                        }
-                        origin.close();
+                        srcFiles.add(pngImage.toString());
                         i++;
                         break;
                     case 5:
                         File jpgImage = new File("D:/Desktop/imageFromWord" + i + ".jpg");
                         ImageIO.write(imag, "jpg", jpgImage);
-                        FileInputStream fi1 = new FileInputStream(jpgImage);
-                        origin = new BufferedInputStream(fi1, BUFFER);
-                        ZipEntry entry1 = new ZipEntry(jpgImage.toString());
-                        out.putNextEntry(entry1);
-                        int count1;
-                        while ((count1 = origin.read(data, 0,
-                                BUFFER)) != -1) {
-                            out.write(data, 0, count1);
-                        }
-                        origin.close();
+                        srcFiles.add(jpgImage.toString());
                         i++;
                         break;
                     default:
                         break;
                 }
             }
-            out.close();
+            // adding images to archive then downloading it on disk
+            addImagesToArchive(srcFiles);
         }
         catch(Exception e) {
             System.exit(-1);
+        }
+    }
+
+    public static void addImagesToArchive(List<String> srcFiles) {
+        try {
+            // initializing the zip archive
+            byte[] BUFFER = new byte[1024];
+            BufferedInputStream origin = null;
+            FileOutputStream dest = new
+                    FileOutputStream("D:/Desktop/imagesArchive.zip");
+            ZipOutputStream out = new ZipOutputStream(dest);
+            // looping through each image stored, adding it to the zip
+            for (String image : srcFiles) {
+                File srcFile = new File(image);
+                FileInputStream fis = new FileInputStream(srcFile);
+                out.putNextEntry(new ZipEntry(srcFile.getName()));
+                int length;
+                while ((length = fis.read(BUFFER)) > 0) {
+                    out.write(BUFFER, 0, length);
+                }
+                out.closeEntry();
+                fis.close();
+            }
+            out.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error creating zip file: " + e);
         }
     }
 }
