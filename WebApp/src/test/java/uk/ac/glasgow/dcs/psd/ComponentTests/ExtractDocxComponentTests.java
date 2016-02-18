ed3200e8f4709aa392ffa9c5d7a33b020a8443ca
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-02-18 12:34:33 +0000

    Ticket #130 - Added a new JUnit test for ExtractDocxComponent and a Resources file structure

--- WebApp/src/test/java/uk/ac/glasgow/dcs/psd/ComponentTests/ExtractDocxComponentTests.java
+++ WebApp/src/test/java/uk/ac/glasgow/dcs/psd/ComponentTests/ExtractDocxComponentTests.java
@@ -4,27 +4,83 @@ package uk.ac.glasgow.dcs.psd.ComponentTests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
+import uk.ac.glasgow.dcs.psd.Components.HelperComponent;
+import uk.ac.glasgow.dcs.psd.Components.ExtractDocxComponent;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class ExtractDocxComponentTests {
 
     String fileName;
+    String output;
+    String testCsv;
 
     @Before
     public void setUp(){
-        fileName = "TestResources/test.docx";
+
+        String directory = System.getProperty("user.dir");
+        directory += "/src/test/java/uk/ac/glasgow/dcs/psd";
+        fileName = directory + "/Resources/test.docx";
+        output = directory + "/Resources/test";
+        testCsv = directory + "/Resources/test.csv";
+
     }
 
 
     @After
-    public void tearDown(){}
+    public void tearDown(){
+        try {
+            HelperComponent.delete(new File(output+".zip"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
     @Test
-    public void getTablesTest(){
+    public void extractTablesTest(){
+        System.out.printf("\n\n\n\n");
+        BufferedReader testReader = null;
+        BufferedReader zipReader = null;
+
+        try {
+            testReader = new BufferedReader(new FileReader(testCsv));
+        }catch (FileNotFoundException e){
+            System.out.printf("test.csv file was not found\n");
+            e.printStackTrace();
+            assert(false);
+        }
+
+        ExtractDocxComponent.extractTablesAndImages(fileName, output);
+        try {
+            ZipFile zip = new ZipFile(output+".zip");
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            ZipEntry entry;
+            while(entries.hasMoreElements()){
+                entry = entries.nextElement();
+                System.out.println(entry.getName());
+                if (entry.getName().endsWith(".csv")) {
+                    zipReader = new BufferedReader(new InputStreamReader(zip.getInputStream(entry)));
+                    String testLine;
+                    while((testLine = testReader.readLine()) != null) {
+                        String zipLine = zipReader.readLine();
+                        System.out.printf("Test: %s\n Zip:%s\n", testLine, zipLine);
+                        assertEquals(testLine, zipLine);
+                    }
+                }
+            }
+            zipReader.close();
+            testReader.close();
+
+        }catch (IOException e){
+            System.out.printf("Error opening zip file %s\n", output);
+            e.printStackTrace();
+            assert(false);
+        }
+
 
     }
 }
