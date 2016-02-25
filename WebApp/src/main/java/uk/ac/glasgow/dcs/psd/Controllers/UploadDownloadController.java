48e63cf8c743f2493b1b4232eeb1b2cb16c94aa9
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-02-25 13:31:38 +0000

    Ticket #152 - Fix tabula maven dependency

--- WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Controllers/UploadDownloadController.java
+++ WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Controllers/UploadDownloadController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uk.ac.glasgow.dcs.psd.Components.*;
 import uk.ac.glasgow.dcs.psd.Models.DownloadZip;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URL;
