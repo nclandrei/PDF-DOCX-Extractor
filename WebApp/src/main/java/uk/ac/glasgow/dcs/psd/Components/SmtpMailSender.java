a95a9e2cfe559f3e55f53ca0b90f7fd7f38ac274
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-14 14:58:14 +0000

    Ticket #187 - Fixed some checkstyle errors for SmtpMailSender, by tidying up the JavaDoc

--- WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/SmtpMailSender.java
+++ WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/SmtpMailSender.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 /**
- * Component to send emails using Gmail API and java MailSender
+ * Component to send emails using Gmail API and java MailSender.
  */
 
 @Component
@@ -19,11 +19,12 @@ public class SmtpMailSender {
     private JavaMailSender javaMailSender;
 
     /**
-     * <h1>Send email</h1> Allows to send email using Gmail
+     * <h1>Send email</h1> Allows to send email using Gmail.
      *
      * @param to      address to send email to
      * @param subject subject of the email message
      * @param body    body of the email message
+     * @throws MessagingException ca throw an exception if sending fails
      */
     public void send(String to, String subject, String body)
             throws MessagingException {
