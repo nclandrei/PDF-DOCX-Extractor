f50d795d96bef46af416301330bbbad6e3bd73be
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-14 14:53:35 +0000

    Ticket #187 - Fixed some checkstyle errors for SmtpMailSender, by running an general editor level reformat

--- WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/SmtpMailSender.java
+++ WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/SmtpMailSender.java
@@ -9,36 +9,36 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 /**
- * Component to send emails using Gmail API
- * and java MailSender
+ * Component to send emails using Gmail API and java MailSender
  */
 
 @Component
 public class SmtpMailSender {
 
-	@Autowired
-	private JavaMailSender javaMailSender;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     /**
-     * <h1>Send email</h1>
-     * Allows to send email using Gmail
+     * <h1>Send email</h1> Allows to send email using Gmail
      *
-     * @param to            address to send email to
-     * @param subject       subject of the email message
-     * @param body          body of the email message
+     * @param to      address to send email to
+     * @param subject subject of the email message
+     * @param body    body of the email message
      */
-	public void send(String to, String subject, String body) throws MessagingException {
+    public void send(String to, String subject, String body)
+            throws MessagingException {
 
-		MimeMessage message = javaMailSender.createMimeMessage();
-		MimeMessageHelper helper;
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
 
-		helper = new MimeMessageHelper(message, true); // true indicates
-		// multipart message
-		helper.setSubject(subject);
-		helper.setTo(to);
-		helper.setText(body, true); // true indicates html
-		// continue using helper object for more functionalities like adding attachments, etc.  
+        helper = new MimeMessageHelper(message, true); // true indicates
+        // multipart message
+        helper.setSubject(subject);
+        helper.setTo(to);
+        helper.setText(body, true); // true indicates html
+        // continue using helper object for more functionalities like adding
+        // attachments, etc.
 
         javaMailSender.send(message);
-	}
+    }
 }
