42bbcd149caeb73758cfa5d0afb311ccb80436c6
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-14 13:37:09 +0000

    Ticket #181 - Refactored the code style of HelperComponent further

--- WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/HelperComponent.java
+++ WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/HelperComponent.java
@@ -16,19 +16,17 @@ import java.io.IOException;
 public class HelperComponent {
     /**
      * Deletes the directory created with tables, images and README file
+     *
      * @param file
      * @throws IOException
      */
-    public static void delete(File file)
-            throws IOException {
+    public static void delete(File file) throws IOException {
 
-        if(file.isDirectory()){
-
+        if (file.isDirectory()) {
             //directory is empty, then delete it
-            if(file.list().length==0){
+            if (file.list().length == 0) {
                 file.delete();
-            }
-            else{
+            } else {
                 //list all the directory contents
                 String files[] = file.list();
 
@@ -39,16 +37,13 @@ public class HelperComponent {
                     //recursive delete
                     delete(fileDelete);
                 }
-
                 //check the directory again, if empty then delete it
-                if(file.list().length==0){
+                if (file.list().length == 0) {
                     file.delete();
-                    System.out.println("Directory is deleted : "
-                            + file.getAbsolutePath());
+                    System.out.println("Directory is deleted : " + file.getAbsolutePath());
                 }
             }
-
-        }else{
+        } else {
             //if file, then delete it
             file.delete();
         }
@@ -57,6 +52,7 @@ public class HelperComponent {
     /**
      * Method that returns the full path of a file provided
      * as a parameter
+     *
      * @param fileName
      * @return
      */
@@ -64,7 +60,7 @@ public class HelperComponent {
         String separator = System.getProperty("file.separator");
         return (System.getProperty("user.dir") + separator) +
                 String.format("src%smain%sresources%sstatic%suploads%s%s",
-                        separator, separator, separator, separator, separator,fileName);
+                        separator, separator, separator, separator, separator, fileName);
     }
 
 }
