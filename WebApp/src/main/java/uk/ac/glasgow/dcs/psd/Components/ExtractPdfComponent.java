db9e05f29f97b3d81804ecfd4d40cd209a4e065e
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-14 13:57:25 +0000

    Ticket #166 - Ran an editor level reformat, fixing line length

--- WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/ExtractPdfComponent.java
+++ WebApp/src/main/java/uk/ac/glasgow/dcs/psd/Components/ExtractPdfComponent.java
@@ -12,6 +12,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Component;
+
 import java.io.FileWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -22,10 +23,9 @@ import java.io.PrintStream;
 import java.io.BufferedWriter;
 
 /**
- * This class provides an API for extracting information from tables
- * into CSVs as well as extracting the images.
- * The CSVs and images are then placed into separate folders and a zip with
- * both of them is returned.
+ * This class provides an API for extracting information from tables into CSVs
+ * as well as extracting the images. The CSVs and images are then placed into
+ * separate folders and a zip with both of them is returned.
  */
 
 @Component
@@ -34,13 +34,15 @@ public class ExtractPdfComponent {
     /**
      * <h1>Extract Tables and Images from a pdf</h1>
      * <p>
-     * The process function combines the functionality of a number of helper functions
-     * that make use of the Tabula jar to generate a JSON file and convert that into a CSV.
-     * The Image extraction tool is then used to provide all of the images from the pdf.
-     * The resulting CSVs and images are then placed into a zip using the ZipMaker utility
-     * and the original files are deleted.
+     * The process function combines the functionality of a number of helper
+     * functions that make use of the Tabula jar to generate a JSON file and
+     * convert that into a CSV. The Image extraction tool is then used to
+     * provide all of the images from the pdf. The resulting CSVs and images are
+     * then placed into a zip using the ZipMaker utility and the original files
+     * are deleted.
      *
-     * @param fileName the file name for the pdf to extract the tables from (without the extension)
+     * @param fileName the file name for the pdf to extract the tables from
+     *                 (without the extension)
      */
 
     public static void process(String fileName) {
@@ -58,10 +60,11 @@ public class ExtractPdfComponent {
     /**
      * <h1>Generates a JSON with the Table data</h1>
      * <p>
-     * generateJSON makes use of Tabula to generate a JSON file
-     * with all the data from the tables in the PDF
+     * generateJSON makes use of Tabula to generate a JSON file with all the
+     * data from the tables in the PDF
      *
-     * @param fileName the file name for the pdf to extract the tables from (without the extension)
+     * @param fileName the file name for the pdf to extract the tables from
+     *                 (without the extension)
      */
 
     private static void generateJSON(String fileName) {
@@ -73,9 +76,11 @@ public class ExtractPdfComponent {
         GnuParser parser = new GnuParser();
         try {
             PrintStream ps = new PrintStream(new File(jsonFile));
-            CommandLine exp = parser.parse(CommandLineApp.buildOptions(), tabulaArgs);
+            CommandLine exp =
+                    parser.parse(CommandLineApp.buildOptions(), tabulaArgs);
             if (exp.getArgs().length != 1) {
-                throw new ParseException("Need one filename\nTry --help for help");
+                throw new ParseException(
+                        "Need one filename\nTry --help for help");
             }
             (new CommandLineApp(ps)).extractTables(exp);
             ps.flush();
@@ -89,10 +94,11 @@ public class ExtractPdfComponent {
     /**
      * <h1>Generates CSVs from a JSON</h1>
      * <p>
-     * processJSON makes use of a json file to generate a folder
-     * of CSVs where each csv holds the extracted information for one table
+     * processJSON makes use of a json file to generate a folder of CSVs where
+     * each csv holds the extracted information for one table
      *
-     * @param fileName the file name of the JSON file used for processing (without the extension)
+     * @param fileName the file name of the JSON file used for processing
+     *                 (without the extension)
      */
 
     private static void processJSON(String fileName) {
@@ -118,7 +124,9 @@ public class ExtractPdfComponent {
                     for (Object p : cellProperties) {
                         JSONObject property = (JSONObject) p;
                         String text = (String) property.get("text");
-                        if (text.compareTo(" ") == 0 || text.compareTo("") == 0 || text.length() > 100) {
+                        if (text.compareTo(" ") == 0 ||
+                                text.compareTo("") == 0 ||
+                                text.length() > 100) {
                             break;
                         }
                         sb.append("\"").append(text).append("\",");
@@ -131,8 +139,10 @@ public class ExtractPdfComponent {
                 }
 
                 if (!sb.toString().isEmpty()) {
-                    try (BufferedWriter buffer = new BufferedWriter(new FileWriter(fileName +
-                            File.separator + "table" + csvName + ".csv"))) {
+                    try (BufferedWriter buffer = new BufferedWriter(
+                            new FileWriter(fileName +
+                                    File.separator + "table" + csvName +
+                                    ".csv"))) {
                         sb.deleteCharAt(sb.length() - 1);
                         sb.deleteCharAt(sb.length() - 1);
                         buffer.write(sb.toString());
@@ -157,7 +167,8 @@ public class ExtractPdfComponent {
      * <p>
      * extractImages provides the images in a PDF as standalone files
      *
-     * @param fileName the file name for the pdf to extract the tables from (without the extension)
+     * @param fileName the file name for the pdf to extract the tables from
+     *                 (without the extension)
      */
 
     private static void extractImages(String fileName) {
@@ -183,8 +194,11 @@ public class ExtractPdfComponent {
 
                     for (Object o : pageImages.keySet()) {
                         String key = (String) o;
-                        PDXObjectImage pdxObjectImage = (PDXObjectImage) pageImages.get(key);
-                        pdxObjectImage.write2file(fileName + File.separator + imageName + totalImages);
+                        PDXObjectImage pdxObjectImage =
+                                (PDXObjectImage) pageImages.get(key);
+                        pdxObjectImage.write2file(
+                                fileName + File.separator + imageName +
+                                        totalImages);
                         totalImages++;
                     }
                 }
