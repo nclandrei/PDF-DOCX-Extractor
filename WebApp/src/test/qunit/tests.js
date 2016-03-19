a07be9c0f6ca8b28865ce1ae9b15f4a54d7abdd3
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-03-19 14:53:59 +0000

    Ticket #194 - Sample qUnit tests included along with configuration to pull the main html file into qUnit

--- WebApp/src/test/qunit/tests.js
+++ WebApp/src/test/qunit/tests.js
@@ -1,3 +1,50 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.begin(function( details ) {
+  console.log( "Test amount:", details.totalTests );
+});
+
+/*// Not using asyncTest as it will be deprecated soon
+function setupModule() {
+  $('input[name="text"]').on('click', function() {
+       $('.has-error').hide();
+  })
+  $('input[name="text"]').on('keypress', function() {
+      $('.has-error').hide();
+  });
+}
+
+module('tests', {setup:setupModule});
+
+test("errors should be hidden on key press", function() {
+  $('input[name="text"]').trigger('keypress')
+  equal($('.has-error').is(':visible'), false);
+});
+
+test("errors not be hidden unless there is a keypress", function() {
+  equal($('.has-error').is(':visible'), true);
+});
+
+test("errors should be hidden on click", function() {
+  $('input[name="text"]').click()
+  equal($('.has-error').is(':visible'),false);
+});*/
+
+QUnit.test( "multiple call done()", function( assert ) {
+  assert.expect( 3 );
+  var done = assert.async( 3 );
+
+  setTimeout(function() {
+  //var button = Dropbox.createChooseButton({})
+    assert.ok( true, "first call done." );
+    done();
+  }, 500 );
+
+  setTimeout(function() {
+    assert.ok( true, "second call done." );
+    done();
+  }, 1500 );
+
+  setTimeout(function() {
+    assert.ok( true, "third call done." );
+    done();
+  }, 2500 );
 });
